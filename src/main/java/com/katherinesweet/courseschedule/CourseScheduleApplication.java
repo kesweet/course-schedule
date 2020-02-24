d9e8dc5692938721e037a41bc6799f138076fd39
Katherine Sweet <katherine.sweet@slu.edu>
2020-02-23 19:49:46 -0600

    set up MySQL, Bootstrap, ran tests and can bootrun with no errors

---
+++ src/main/java/com/katherinesweet/courseschedule/CourseScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.katherinesweet.courseschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseScheduleApplication.class, args);
+	}
+
+}
